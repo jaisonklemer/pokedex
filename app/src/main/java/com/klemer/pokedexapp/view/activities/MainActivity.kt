02ee20432c51a6f2bb36d74ef29f892d580ebc89
Jaison Klemer <klemerjaison@gmail.com>
2021-08-20 13:54:29 -0300

    remove filter logic from MainActivity

--- app/src/main/java/com/klemer/pokedexapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/klemer/pokedexapp/view/activities/MainActivity.kt
@@ -2,15 +2,7 @@ package com.klemer.pokedexapp.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageView
-import androidx.core.content.ContextCompat
-import androidx.core.view.get
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.klemer.pokedexapp.R
-import com.klemer.pokedexapp.adapters.GenBottomModalAdapter
 import com.klemer.pokedexapp.databinding.MainActivityBinding
 import com.klemer.pokedexapp.view.fragments.MainFragment
 
@@ -18,9 +10,6 @@ import com.klemer.pokedexapp.view.fragments.MainFragment
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: MainActivityBinding
-    private lateinit var bottomSheetView: View
-    private lateinit var bottomSheetRecyclerView: RecyclerView
-    private lateinit var bottomSheetDialog: BottomSheetDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,29 +22,5 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
-
-        loadFilterComponents()
-
-        findViewById<ImageView>(R.id.imgGenerationFilter).setOnClickListener {
-            bottomSheetDialog.show()
-        }
     }
-
-    fun loadFilterComponents() {
-        bottomSheetView = View.inflate(this, R.layout.bottom_modal_generations, null)
-        bottomSheetRecyclerView =
-            bottomSheetView.findViewById(R.id.recyclerViewGenerations)
-
-        bottomSheetRecyclerView.adapter = GenBottomModalAdapter(){
-            println("Generation item $it")
-        }
-        bottomSheetRecyclerView.layoutManager = GridLayoutManager(this, 2)
-
-        /*
-        * Bottom Sheet Dialog for Generations Filter
-        * */
-        bottomSheetDialog = BottomSheetDialog(this)
-        bottomSheetDialog.setContentView(bottomSheetView)
-    }
-
 }
